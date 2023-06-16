431caec26256fff2520090987fdc7c7a572f1049
masmahardi <64582990+mahardisetyoso@users.noreply.github.com>
2023-06-17 00:24:16 +0700

    Update app.py streamlit_pages

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import streamlit as st
+from streamlit_pages import Page, Section, add_page_title, show_pages
 
-with st.echo("below"):
-    from st_pages import Page, Section, add_page_title, show_pages
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+st.write("# Welcome to Streamlit! 👋")
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    show_pages(
-        [
-            Page("app_0.py","Home","🏠"),
-            Page("app_1.py","Map","🗺️"),
-            Page("app_2.py","Charts","📊")
-        ]
-    )
+show_pages(
+     [
+        Page("app_0.py","Home","🏠"),
+        Page("app_1.py","Map","🗺️"),
+        Page("app_2.py","Charts","📊")
+    ]
+ )
