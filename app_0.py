45542b2e4d4ef32f1a8499f5ae665e4982c87e57
masmahardi <64582990+mahardisetyoso@users.noreply.github.com>
2023-06-17 11:25:40 +0700

    Update app_0.py minor update

--- app_0.py
+++ app_0.py
@@ -10,9 +10,9 @@ st.write('# TABLE')
 
 st.write('Below you will fin some table that will be generated as ***MAP*** and also ***CHART*** one next tab')
 
-xls = pd.ExcelFile(r"C:\Users\Mahardi\OneDrive\Documents\python_project\Streamlit Simple Dashboard\data\Macroeconomy Data for Territory_.xlsx")
-
-df1 = pd.read_excel(xls,'City Level Data')
+url = 'https://github.com/mahardisetyoso/streamlit_demo_hardy/raw/main/data/Macroeconomy%20Data%20for%20Territory_.xlsx'
+xlsx = requests.get(url).content
+df1 = pd.read_excel(BytesIO(xlsx),'City Level Data')
 
 st.write('Displaying the dataframe: `df`')
 st.write(df1.head())
